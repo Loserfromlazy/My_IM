039427bd8f5d15f7b723004963c4eceb41c417dc
yhr <yuhaoran@winten.cn>
2022-06-06 16:37:31 +0800

    init

---
+++ IM_Monomer/src/test/java/com/learn/im_monomer/ImMonomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.im_monomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImMonomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
