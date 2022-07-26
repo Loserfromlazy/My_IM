ff46d5a9c8601d5ceb55856e6ed6cfbe3cdbcc2a
yhr <yuhaoran@winten.cn>
2022-07-26 15:42:29 +0800

    更新服务端代码

--- server/src/main/java/com/myim/server/ServerApplication.java
+++ server/src/main/java/com/myim/server/ServerApplication.java
@@ -4,15 +4,16 @@ import com.myim.server.start.IMServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class ServerApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(ServerApplication.class, args);
-        final IMServer imServer = (IMServer) context.getBean("IMServer");
-
-
+        IMServer imServer = (IMServer) context.getBean("IMServer");
+        imServer.start();
     }
 
 }
--- server/src/main/java/com/myim/server/start/IMServer.java
+++ server/src/main/java/com/myim/server/start/IMServer.java
@@ -1,9 +1,21 @@
 package com.myim.server.start;
 
+import com.myim.common.codec.SimpleProtobufDecoder;
+import com.myim.common.codec.SimpleProtobufEncoder;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.net.InetSocketAddress;
 
 /**
  * <p>
@@ -13,15 +25,48 @@ import org.springframework.stereotype.Component;
  * @author Yuhaoran
  * @since 2022/7/25
  */
-@Component("IMServer")
+@Service("IMServer")
+@Slf4j
 public class IMServer {
 
 
-    private ServerBootstrap bootstrap;
+    @Value("${server.port}")
+    private Integer port;
+
+    private ServerBootstrap bootstrap = new ServerBootstrap();
     private EventLoopGroup bossGroup;
     private EventLoopGroup workerGroup;
-    public void start(){
-        bootstrap.channel(NioServerSocketChannel.class);
+
+    public void start() {
+        bossGroup = new NioEventLoopGroup();
+        workerGroup = new NioEventLoopGroup();
+
+        try {
+            bootstrap
+                    .group(bossGroup, workerGroup)
+                    .localAddress(new InetSocketAddress(port))
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                    .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new SimpleProtobufDecoder());
+                            socketChannel.pipeline().addLast(new SimpleProtobufEncoder());
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.bind();
+            channelFuture.sync();
+            log.info("服务启动，正在监听端口{}", port);
+            ChannelFuture closeFuture = channelFuture.channel().closeFuture();
+            closeFuture.sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
     }
 
 }
