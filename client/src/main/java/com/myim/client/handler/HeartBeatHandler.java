fbf23e26796f23457850d840b1491ac96511e12b
Loserfromlazy <1320064431@qq.com>
2022-08-01 22:24:53 +0800

    1

--- client/src/main/java/com/myim/client/handler/HeartBeatHandler.java
+++ client/src/main/java/com/myim/client/handler/HeartBeatHandler.java
@@ -1,16 +1,24 @@
 package com.myim.client.handler;
 
 import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
 @ChannelHandler.Sharable
 public class HeartBeatHandler extends ChannelInboundHandlerAdapter {
 
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+
+        super.handlerAdded(ctx);
+    }
 }
